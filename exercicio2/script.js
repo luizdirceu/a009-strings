aa77ed56beb2e58f88d82888b042b5bfd5b2dc19
Luiz Dirceu <juninholdmarques@gmail.com>
2022-07-26 18:27:31 -0300

    minha atividade 009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,7 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// const string = "Eu não sou supersticioso, mas sou um pouco sticioso."
+
+console.log (minhaString.trim())
+console.log (minhaString.length)
+console.log (minhaString.trim().length)
+console.log (minhaString.replace("________","sticioso"))
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
 //Crie a const para a frase aqui
+
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: 'BOAS VINDAS, mas não deixe o gato sair'"
+console.log (frase.replaceAll("verde","rosa",) .replaceAll ("azul","laranja"))
+
+const temverde = frase.includes("verde")
+console.log (temverde)
+
+const temlaranja = frase.includes("laranja")
+console.log (temlaranja)
